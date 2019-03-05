a00f561794996da90a0b520fad68d14d1f892105
anacamargo <anaclaudia2501@gmail.com>
2019-03-05 02:11:51 -0300

    Mudança de visual e inclusão de mais filtros

--- src/main.js
+++ src/main.js
@@ -14,11 +14,14 @@ async function init() { // Criei essa função assíncrona para usar o await e a
             img: champion.img,
             splash: champion.splash,
             blurb: champion.blurb,
-            tags: champion.tags
+            tags: champion.tags,
+            attack: champion.info.attack,
+            defense: champion.info.defense,
+            magic: champion.info.magic
         }
     });
 
-    document.querySelector('#sort').addEventListener('change', buttonClicked); 
+    document.querySelector('#sort').addEventListener('change', buttonClicked);
     document.querySelector('#operator').addEventListener('change', buttonClicked);
 
     let buttons = document.querySelectorAll('button');
@@ -60,24 +63,58 @@ async function init() { // Criei essa função assíncrona para usar o await e a
     function sort(filtered) {
         const select = document.getElementById('sort');
         const selectedValue = select.options[select.selectedIndex].value;
-        if (selectedValue === 'ZA') show(filtered.slice().reverse()); // slice matém o array original
-        else show(filtered);
+        switch (selectedValue) {
+            case 'AZ':
+                show(filtered);
+                break;
+            case 'ZA':
+                show(filtered.slice().reverse());
+                break;
+            case 'Attack':
+                show(filtered.slice().sort(
+                    (a, b) => {
+                        if (a.attack > b.attack) return 1;
+                        else if (a.attack < b.attack) return -1;
+                        else return 0;
+                    }
+                ).reverse());
+                break;
+            case 'Defense':
+                show(filtered.slice().sort(
+                    (a, b) => {
+                        if (a.defense > b.defense) return 1;
+                        else if (a.defense < b.defense) return -1;
+                        else return 0;
+                    }
+                ).reverse());
+                break;
+            case 'Magic':
+                show(filtered.slice().sort(
+                    (a, b) => {
+                        if (a.magic > b.magic) return 1;
+                        else if (a.magic < b.magic) return -1;
+                        else return 0;
+                    }
+                ).reverse());
+                break;
+        }
+
+
     }
 
     function show(data) {
-        let list = document.querySelector('#champion-list'); // seleciona o ul
-        return list.innerHTML = `${data.map((champion) => //incrementa o li para cada champion
+        let list = document.querySelector('#champion-list');
+        return list.innerHTML = `${data.map((champion) =>
             `<li class="item">
-                <figure class="container-image">
-                    <img class="image" src="image/${champion.id}.jpg" alt="${champion.name}"/>
-                </figure>
-                <h2 class="name">${champion.name}</h2>
-                ${champion.tags.map(x => '<span class="tag">' + x + '<span/>').join("\n")}
+                <div class="container-image" style="background-image:url(image/${champion.id}.jpg)">
+                    <h2 class="name">${champion.name}</h2>
+                    <div class="classes">
+                        ${champion.tags.map(x => '<span class="tag">' + x + '<span/>').join(" | ")}
+                    </div>
+                </div>
             </li>`).join("")}`
     }
 
     show(data);
-
-
 }
 init();

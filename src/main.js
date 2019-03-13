50aa7545653ca3cae7b85099d0a685c0d85c5cd8
anacamargo <anaclaudia2501@gmail.com>
2019-03-12 22:30:09 -0300

    Alteração de pasta do index, correção dos caminhos e inclusão do readme

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 async function init() {
-    const fileContent = await fetch('data/lol/lol.json');
+    const fileContent = await fetch('src/data/lol/lol.json');
     const json = (await fileContent.json()).data;
 
     const champions = Object.keys(json).reduce((list, champion) => {
         return list.concat(json[champion]);
     }, []);
 
-    const data = champions.map((champion) => { 
+    const data = champions.map((champion) => {
         return {
             id: champion.id,
             name: champion.name,
@@ -19,15 +19,15 @@ async function init() {
             defense: champion.info.defense,
             magic: champion.info.magic,
             stats: [
-                {css: 'icon-hp', name: 'HP', value: champion.stats.hp, perLevel: champion.stats.hpperlevel},
-                {css: 'icon-regen', name: 'HP Regeneration', value: champion.stats.hpregen, perLevel: champion.stats.hpregenperlevel},
-                {css: 'icon-dmg', name: 'Attack Damage',value: champion.stats.attackdamage, perLevel: champion.stats.attackdamageperlevel},
-                {css: 'icon-atk-spd', name: 'Attack Speed',value: champion.stats.attackspeedoffset, perLevel: champion.stats.attackspeedperlevel},
-                {css: 'icon-spd', name: 'Speed', value: champion.stats.movespeed, perLevel: champion.stats.mpperlevel},
-                {css: 'icon-armor', name: 'Armor', value: champion.stats.armor, perLevel: champion.stats.armorperlevel},
-                {css: 'icon-spell-res', name: 'Spell Resistance', value: champion.stats.spellblock, perLevel: champion.stats.spellblockperlevel},
-                {css: 'icon-mp', name: 'MP', value: champion.stats.mp, perLevel: champion.stats.mpperlevel},
-                {css: 'icon-mp-regen', name: 'MP Regeneration', value: champion.stats.mpregen, perLevel: champion.stats.mpregenperlevel}
+                { css: 'icon-hp', name: 'HP', value: champion.stats.hp, perLevel: champion.stats.hpperlevel },
+                { css: 'icon-regen', name: 'HP Regeneration', value: champion.stats.hpregen, perLevel: champion.stats.hpregenperlevel },
+                { css: 'icon-dmg', name: 'Attack Damage', value: champion.stats.attackdamage, perLevel: champion.stats.attackdamageperlevel },
+                { css: 'icon-atk-spd', name: 'Attack Speed', value: champion.stats.attackspeedoffset, perLevel: champion.stats.attackspeedperlevel },
+                { css: 'icon-spd', name: 'Speed', value: champion.stats.movespeed, perLevel: champion.stats.mpperlevel },
+                { css: 'icon-armor', name: 'Armor', value: champion.stats.armor, perLevel: champion.stats.armorperlevel },
+                { css: 'icon-spell-res', name: 'Spell Resistance', value: champion.stats.spellblock, perLevel: champion.stats.spellblockperlevel },
+                { css: 'icon-mp', name: 'MP', value: champion.stats.mp, perLevel: champion.stats.mpperlevel },
+                { css: 'icon-mp-regen', name: 'MP Regeneration', value: champion.stats.mpregen, perLevel: champion.stats.mpregenperlevel }
             ]
         }
     });
@@ -116,22 +116,23 @@ async function init() {
         return list.innerHTML = `${data.map((champion) =>
             `<li class="flip-container" onclick="this.classList.toggle('hover');">
                 <div class="flipper">
-                    <div class="front container-image" style="background-image:url(image/${champion.id}.jpg)">
-                        <h2 class="name">${champion.name}</h2>
+                    <div class="front container-image" style="background-image:url(src/image/${champion.id}.jpg)">
+                        <h2 class="name" data-chart="${champion.name}">${champion.name}</h2>
                         <div class="classes">
                             ${champion.tags.map(x => '<span>' + x + '<span/>').join(" | ")}
                         </div>
                     </div>
-                    <div class="back" style="background-image:url(image/bg-back.png)">
-                        ${champion.stats.map(stat => 
-                            `<span title="${stat.name}" class="stats ${stat.css}">
+                    <div class="back" style="background-image:url(src/image/bg-back.png)">
+                        ${champion.stats.map(stat =>
+                `<span title="${stat.name}" class="stats ${stat.css}">
                                 <span>${stat.value} (${stat.perLevel}/lvl)</span>
                             </span>`).join('\n')}
                     </div>  
                 </div>
             </li>`).join("")}`
     }
-
+    
     show(data);
 }
+
 init();
